96a3ee12b055484f1ef7a96ca1bb7336ca5c9577
PabloMedinaForero <p.medina1@uniandes.edu.co>
2025-05-13 21:24:53 -0500

    agregar prueba de atraciones

---
+++ src/test/atracciones/TestAtraccion.java
@@ -0,0 +1,13 @@
+package test.atracciones;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestAtraccion {
+
+	
+	
+	
+	
+}
